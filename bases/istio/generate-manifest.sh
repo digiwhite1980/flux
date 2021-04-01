539005752d7b65f172d8427a945bcb0cc709660b
Raymond Otto <Raymond.Otto@ing.com>
2021-04-01 10:34:31 +0200

    Initial dev push

--- bases/istio/generate-manifest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker container run \
-  --name istioctl \
-  --rm \
-  -v "$PWD":"$PWD" \
-  -w "$PWD" \
-  istio/istioctl:1.6.1 \
-    manifest -f custom-config.yaml generate > ./generated-manifest.yaml
---
+++ docu/README.md
@@ -0,0 +1,3 @@
+```
+flux bootstrap github --owner=digiwhite1980 --repository=flux --branch=dev  --path=./cluster/flux  --personal
+```
